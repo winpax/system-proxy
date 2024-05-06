b8f0c62446503b4e0ea5717d39b00ce988486376
Juliette Cordor <professional@maybejules.com>
2024-05-06 22:11:26 +1000

    added files

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    net::{AddrParseError, IpAddr, SocketAddr},
+    str::FromStr,
+};
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("IO error: {0}")]
+    IOError(#[from] std::io::Error),
+    #[error("AddrParse error: {0}")]
+    AddrParse(#[from] AddrParseError),
+    #[error("Missing system proxy configuration")]
+    MissingProxyConfig,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub type Result<T, E = Error> = std::result::Result<T, E>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Clone)]
+pub struct SystemProxy {
+    pub enabled: bool,
+    pub address: IpAddr,
+    pub port: u16,
+}
+
+impl SystemProxy {
+    pub fn get_system_proxy() -> Result<Self> {
+        let hklm = winreg::RegKey::predef(winreg::enums::HKEY_LOCAL_MACHINE);
+        let key = hklm.open_subkey("SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters")?;
+        let enabled = key.get_value("EnableProxy").unwrap_or(0u32) == 1;
+        let server: String = key.get_value("ProxyServer")?;
+
+        let (host, port) = if server.is_empty() {
+            return Err(Error::MissingProxyConfig);
+        } else {
+            let socket = SocketAddr::from_str(&server)?;
+
+            (socket.ip(), socket.port())
+        };
+
+        Ok(Self {
+            enabled,
+            address: host,
+            port,
+        })
     }
 }
